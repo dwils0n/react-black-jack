635333911b315f4ef748eefee0daeb429ec02a88
Dan Wilson <daniel.wilson@spindriftgroup.com>
2017-12-19 10:57:21 +0000

    Update card test

--- src/components/Card/Card.test.js
+++ src/components/Card/Card.test.js
@@ -2,13 +2,21 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Card from './Card';
 
-const props = {
-    cardId: '2',
-    cardSuit: 'diamonds'
-}
+const testSuit = 'diamonds',
+    testCard = '2',
+    props = {
+        cardId: testCard,
+        cardSuit: testSuit
+    };
 
 it('renders correctly', () => {
     const component = shallow(<Card {...props} />);
 
     expect(component.find('.card').length).toBe(1);
+});
+
+it('computeCardClass returns the correct class', () => {
+    const component = shallow(<Card {...props} />);
+
+    expect(component.instance().computeCardClass()).toBe(`card card--${testSuit} card--${testCard}`);
 });
